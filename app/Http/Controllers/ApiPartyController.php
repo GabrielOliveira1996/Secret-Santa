2f7f89cd18ab17a351615d6d1399e111b14c37e1
Gabriel <gab-oliveira96@hotmail.com>
2023-11-26 11:38:21 -0300

    feat: :sparkles: Party creation function
    Now the participants data is captured in an array and sent for validation.

--- app/Http/Controllers/ApiPartyController.php
+++ app/Http/Controllers/ApiPartyController.php
@@ -5,31 +5,37 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Validator\PartyValidation;
+use App\Validator\ParticipantValidation;
 use App\Repository\Party\IPartyRepository;
 
 class ApiPartyController extends Controller
 {
     private $_request;
     private $_partyValidation;
+    private $_participantValidation;
     private $_partyRepository;
 
-    public function __construct(Request $request, PartyValidation $partyValidation, IPartyRepository $partyRepository){
+    public function __construct(Request $request, 
+                                PartyValidation $partyValidation, 
+                                ParticipantValidation $participantValidation, 
+                                IPartyRepository $partyRepository){
         $this->_request = $request;
         $this->_partyValidation = $partyValidation;
         $this->_partyRepository = $partyRepository;
+        $this->_participantValidation = $participantValidation;
     }
 
     public function create(){
         try{
-            $party = $this->_request->only([
-                'date',
-                'location',
-                'maximum_value',
-                'message'
-            ]);
-            $validator = $this->_partyValidation->create($party);
-            if($validator){
-                throw new \Exception(json_encode($validator->messages()));
+            $party = $this->_request->only(['date', 'location', 'maximum_value', 'message']);
+            $partyValidator = $this->_partyValidation->create($party);
+            if($partyValidator){
+                throw new \Exception(json_encode($partyValidator->messages()));
+            }
+            $participants = $this->_request->input('participants');
+            $participantValidator = $this->_participantValidation->create($participants);
+            if($participantValidator){
+                throw new \Exception(json_encode($participantValidator->messages()));      
             }
             $create = $this->_partyRepository->create($party);
             return response()->json([
