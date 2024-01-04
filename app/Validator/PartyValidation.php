e6d874169bdd54b7dfaec6628ea2a2165b6f69ad
Gabriel <gab-oliveira96@hotmail.com>
2024-01-04 11:22:22 -0300

    refactor: :recycle: Update PartyValidation
    'Token' parameter has been removed from party validation.

--- app/Validator/PartyValidation.php
+++ app/Validator/PartyValidation.php
@@ -16,21 +16,18 @@ class PartyValidation{
         $rules = [
             'date' => 'required|date',
             'location' => 'required|string',
-            'maximum_value' => 'required|numeric',
-            'message' => 'max:200',
-            'token' => 'required|min:60|unique:parties'
+            'maximum_value' => 'required|numeric|min:1',
+            'message' => 'max:200'
         ];
         $messages = [
-            'date.required' => 'Necessário escrever a data da festa.',
-            'date.date' => 'Valor precisa ser uma data.',
-            'location.required' => 'Necessário escrever o local da festa.',
-            'location.string' => 'Valor precisa conter apenas letras.',
-            'maximum_value.required' => 'Necessário que seja passado o valor máximo do presente.',
-            'maximum_value.numeric' => 'Valor deve conter números.',
-            'message.max' => 'Mensagem deve conter no máximo 200 caracteres.',
-            'token.required' => 'Necessário passar o token da festa.',
-            'token.min' => 'O token deve ter pelo menos 60 caracteres.',
-            'token.unique' => 'Erro ao gerar token, token deve ser único para festa.'
+            'date.required' => 'Preencha a data da festa.',
+            'date.date' => 'Preencha a data corretamente.',
+            'location.required' => 'Preencha o local da festa.',
+            'location.string' => 'Preencha o local da festa corretamente. Deve ter apenas letras.',
+            'maximum_value.required' => 'Preencha o valor máximo do presente.',
+            'maximum_value.numeric' => 'Valor do presente deve conter apenas números.',
+            'maximum_value.min' => 'Valor do presente precisa ser maior que 0.',
+            'message.max' => 'Mensagem deve conter no máximo 200 caracteres.'
         ];
         $validation = Validator::make($party, $rules, $messages);
         if($validation->fails()){
