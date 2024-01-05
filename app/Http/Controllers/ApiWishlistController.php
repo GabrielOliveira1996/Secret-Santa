615bfb555e24955bf8dbce635bb0d7831f18cd23
Gabriel <gab-oliveira96@hotmail.com>
2024-01-05 12:14:52 -0300

    feat: :sparkles: Created 'ApiWishlistController'
    Wishlist Controller has been started. Initially, the 'create' function was created to receive the data, create the participant's wish list and call the email provider to send the wish list link to the secret friend.

---
+++ app/Http/Controllers/ApiWishlistController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Providers\Mail;
+use App\Models\Wish;
+use App\Models\Participant;
+
+class ApiWishlistController extends Controller
+{
+    private $_timezone;
+    private $_request;
+    private $_mail;
+
+    public function __construct(Request $request, Mail $mail){
+        $this->_timezone = date_default_timezone_set('America/Fortaleza');
+        $this->_request = $request;
+        $this->_mail = $mail;
+    }
+
+    public function create($token){
+        try{
+            $wishes = $this->_request->input('wishes');
+            if(!$wishes){
+                throw new \Exception('Nenhum desejo foi enviado.');
+            }
+            $participant = Participant::where('token', $token)->first();
+            if(!$participant){
+                throw new \Exception('Nenhum participante foi encontrado.');
+            }
+            $createWishlist = $participant->wishes()->createMany($wishes);
+            $secretSanta = Participant::where('party_id', $participant['party_id'])->where('name', $participant['secret_santa'])->first();
+            $sendEmail = $this->_mail->sendWishes($participant, $secretSanta['email'], $secretSanta['token']);
+            return response()->json([
+                'status' => 'success',
+                'party' => $wishes
+            ], 200);
+        }catch(\Exception $e){
+            return response()->json([
+                'status' => 'error',
+                'messages' => $e->getMessage()
+            ], 200);
+        }
+    }
+    
+}
