5b6e116cc6b704396620bb49d591e6e48ec0a76c
Gabriel <gab-oliveira96@hotmail.com>
2024-01-07 17:38:24 -0300

    refactor: :recycle: Updated 'ApiWishlistController'
    The error code has been added to return to the front end in all exceptions. And the 'status' parameters in the returns were removed.

--- app/Http/Controllers/ApiWishlistController.php
+++ app/Http/Controllers/ApiWishlistController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Providers\Mail;
+use App\Interfaces\IMail;
 use App\Models\Wish;
 use App\Models\Participant;
 
@@ -14,7 +14,7 @@ class ApiWishlistController extends Controller
     private $_request;
     private $_mail;
 
-    public function __construct(Request $request, Mail $mail){
+    public function __construct(Request $request, IMail $mail){
         $this->_timezone = date_default_timezone_set('America/Fortaleza');
         $this->_request = $request;
         $this->_mail = $mail;
@@ -24,24 +24,22 @@ class ApiWishlistController extends Controller
         try{
             $wishes = $this->_request->input('wishes');
             if(!$wishes){
-                throw new \Exception('Nenhum desejo foi enviado.');
+                throw new \Exception('Nenhum desejo foi enviado.', 400);
             }
             $participant = Participant::where('token', $token)->first();
             if(!$participant){
-                throw new \Exception('Nenhum participante foi encontrado.');
+                throw new \Exception('Nenhum participante foi encontrado.', 400);
             }
             $createWishlist = $participant->wishes()->createMany($wishes);
             $secretSanta = Participant::where('party_id', $participant['party_id'])->where('name', $participant['secret_santa'])->first();
-            $sendEmail = $this->_mail->sendWishes($participant, $secretSanta['email'], $token);
+            $sendEmail = $this->_mail->sendWishList($participant['name'], $secretSanta['email'], $token);
             return response()->json([
-                'status' => 'success',
-                'party' => $wishes
+                'wishes' => $wishes
             ], 200);
         }catch(\Exception $e){
             return response()->json([
-                'status' => 'error',
-                'messages' => $e->getMessage()
-            ], 200);
+                'message' => $e->getMessage()
+            ], $e->getCode());
         }
     }
     
